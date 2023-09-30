07c7c5980a3356043cc2241ed908eeb65cc3bdef
DorianSnowball <mail@doriansnowball.de>
2023-09-30 13:35:21 +0200

    add Delay to DoubleJump

--- extensions/DoubleJump.gd
+++ extensions/DoubleJump.gd
@@ -1,13 +1,20 @@
 extends Extension
 
-var usedJump = false
+var used_jump = false
+var last_on_floor = Time.get_ticks_msec()
+@export var delay = 100 
+
 
 func handle_action(character: Player, delta):
-	print(usedJump)
 	if !character.is_on_floor():
-		if Input.is_action_just_pressed("jump"):
+		if Input.is_action_just_pressed("jump") && can_use():
 			character.velocity.y -=  character.JUMP_FORCE
 			character._animated_sprite.play("jump")
-			usedJump = true
+			used_jump = true
 	else:
-		usedJump = false
+		used_jump = false
+		last_on_floor = Time.get_ticks_msec()
+		
+func can_use():
+	var diff = (Time.get_ticks_msec() - last_on_floor)
+	return (diff > delay) && !used_jump
