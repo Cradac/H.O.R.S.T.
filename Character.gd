82df4b3cfa177b6658d00bc013e7ca3088991173
Max Dettmann <max.dettmann@web.de>
2023-09-30 12:30:43 +0200

    added sprites and rudimentary animation for movement and jumping.

--- Character.gd
+++ Character.gd
@@ -7,6 +7,8 @@ extends CharacterBody2D
 @export var GRAVITY = 500
 
 @onready var axis = Vector2.ZERO
+@onready var _animated_sprite = $AnimatedSprite2D
+
 
 func _physics_process(delta):
 	move(delta)
@@ -20,12 +22,21 @@ func move(delta):
 	if x == 0:
 		if is_on_floor():
 			apply_friction(FRICTION * delta)
+			_animated_sprite.play("idle")
 	else:
 		velocity.x += x * ACCELERATION * delta
+		_animated_sprite.play("walk")
+		print()
+		if x > 0:
+			_animated_sprite.set_flip_h(false)
+		if x < 0:
+			_animated_sprite.set_flip_h(true)
+		
 	if !is_on_floor():
 		velocity.y += GRAVITY * delta
 	if Input.is_action_just_pressed("jump"):
 		velocity.y -=  JUMP_FORCE
+		_animated_sprite.play("jump")
 		
 	move_and_slide()
 
