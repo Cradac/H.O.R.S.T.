5b3745afe6413667dc5a635e3fceb95aa0a2188e
DorianSnowball <mail@doriansnowball.de>
2023-10-01 15:30:39 +0200

    Provide setter for Skillshard Skill

--- interactables/Skillshard.gd
+++ interactables/Skillshard.gd
@@ -1,14 +1,21 @@
 class_name Skillshard extends Area2D
 
-@export var skill: Skill.Skills = Skill.Skills.OTHER
+@export var skill: Skill.Skills = Skill.Skills.OTHER:
+	set(new_skill):
+		skill = new_skill
+		update_extension()
+		
 @onready var sprite: Sprite2D = $"Sprite2D"
 
 var picked_up = false
 var extension: Extension
 
-func _ready():
+func update_extension():
 	extension = Extension.get_extension(skill).new()
 	sprite.texture = extension.get_texture()
+
+func _ready():
+	update_extension()
 
 func _on_body_entered(body):
 	if !picked_up:
