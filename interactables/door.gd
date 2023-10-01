c3da6aa718624fdc59cb10de4e831f257ef2afc7
Max Dettmann <max.dettmann@web.de>
2023-10-01 03:39:45 +0200

    first level ready for wide testing

--- interactables/door.gd
+++ interactables/door.gd
@@ -20,6 +20,7 @@ func _process(delta):
 func open_door():
 	open = true
 	_animated_sprite.play("open")
+	await get_tree().create_timer(1.0).timeout
 	_static_body_2d.set_collision_layer(0)
 	
 func close_door():
